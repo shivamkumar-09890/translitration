fdfe78975307770eb0e2db7103df9ff8de4696e1
shivamkumar-09890 <shivamkumar878758@gmail.com>
2025-10-18 05:40:26 +0000

    refa:frontend redesigned

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export async function transliterate(inputWord) {
-  const res = await fetch("http://localhost:8000/api/transliterate", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ input_word: inputWord }),
-  });
-  return res.json();
-}
